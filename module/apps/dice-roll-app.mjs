9f4d21b46e7fea6bdb1021ac31a20508e3bd82b5
henriquebot <101667236+henriquebot@users.noreply.github.com>
2026-09-23 21:51:42 -0300

    refactor(i18n): localize default dice roll title

--- module/apps/dice-roll-app.mjs
+++ module/apps/dice-roll-app.mjs
@@ -57,7 +57,7 @@ export class DiceRollApp extends HandlebarsApplicationMixin(ApplicationV2) {
         tag: 'div',
         window: {
             frame: true,
-            title: 'Dice Roll',
+            title: 'MIST_ENGINE.ROLL.Title',
             icon: 'fa-solid fa-book-atlas',
             positioned: true,
             resizable: true
